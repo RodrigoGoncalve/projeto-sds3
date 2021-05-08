56d503499696aa3cb8d349d01b1b3e1d612517d6
Rodrigo Goncalves <ro.goncalve@gmail.com>
2021-05-07 22:06:51 -0300

    Alterando pasta do properties

--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsvendas.service;
 
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
